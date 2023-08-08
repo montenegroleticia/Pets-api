098eaf9071c5006b5fd9f4260d0d423e99e8a488
Letícia Montenegro <montenegro.letiicia@gmail.com>
2023-08-08 11:04:38 -0300

    Controllers CRUD

--- src/routers/petsRouter.ts
+++ src/routers/petsRouter.ts
@@ -1,11 +1,18 @@
 import { Router } from "express";
+import {
+  deletePet,
+  getPetById,
+  getPets,
+  postPets,
+  putPet,
+} from "../Controllers";
 
 const petsRouter = Router();
 
-petsRouter.post("/post");
-petsRouter.get("/pets");
-petsRouter.get("/pets/:id");
-petsRouter.put("/pets/:id");
-petsRouter.delete("/pets/:id");
+petsRouter.post("/post", postPets);
+petsRouter.get("/pets", getPets);
+petsRouter.get("/pets/:id", getPetById);
+petsRouter.put("/pets/:id", putPet);
+petsRouter.delete("/pets/:id", deletePet);
 
 export { petsRouter };
